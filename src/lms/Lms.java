d647f1831af78e8b6e3e10ad56d666cc18f99622
Swapnil Jagdish Prasad Yadav <79936181+swapnilyadv@users.noreply.github.com>
2022-11-02 03:13:21 +0530

    Add files via upload

---
+++ src/lms/Lms.java
@@ -0,0 +1,16 @@
+
+package lms;
+
+import lms.Jframe.SignupPage;
+
+
+
+public class Lms {
+
+  
+    public static void main(String[] args) {
+        new SignupPage().setVisible(true);
+        
+    }
+    
+}
