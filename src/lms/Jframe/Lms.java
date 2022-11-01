16ad46bc7e61244d71134cf29166e0e590f25960
Swapnil Jagdish Prasad Yadav <79936181+swapnilyadv@users.noreply.github.com>
2022-11-02 03:11:58 +0530

    Create Lms.java

---
+++ src/lms/Jframe/Lms.java
@@ -0,0 +1,15 @@
+package lms;
+
+import lms.Jframe.SignupPage;
+
+
+
+public class Lms {
+
+  
+    public static void main(String[] args) {
+        new SignupPage().setVisible(true);
+        
+    }
+    
+}
